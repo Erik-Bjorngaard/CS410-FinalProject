2e7f060e7d9a4d4948577e384fb6d3cdede00e7f
BryanOnofre <onofreb128@gmail.com>
2020-05-07 03:34:55 -0500

    Add route to handle form request from site. Prototype code

--- flaskFile.py
+++ flaskFile.py
@@ -7,6 +7,23 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+# render default index page
+@app.route('/runQuery', methods=['POST'])
+def runQuery():
+    result = "null"
+    subject = request.form['subject']
+    stateAbrv = request.form['state']
+
+    if subject == 1:
+        print(subject, stateAbrv)
+
+    result = getPopulation(stateAbrv)
+    output = f'There are: {result}' + \
+                    f' people in the state of: {stateAbrv}'
+
+    print(output)
+
+    return render_template('index.html', display = output)
 
 
 if __name__ == '__main__':
